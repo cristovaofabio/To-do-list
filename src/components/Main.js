dccfaca6cc4f44967d5c53c0f9f659faafbe41e2
Cristovao <cristovaofabio3@gmail.com>
2022-04-20 10:06:15 -0300

    form style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import './Main.css';
+import { FaPlus } from 'react-icons/fa';
+
+import { FaPencilAlt, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
-    newTask: ''
+    newTask: '',
+    tasks: [
+      'drink coffee',
+      'eat something',
+      'exercise myself',
+      'study'
+    ]
   };
 
   handleChanges = (event) => {
@@ -13,16 +22,30 @@ export default class Main extends Component {
   }
 
   render() {
-    // const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
       <div className="main">
         <h1>Task list</h1>
 
-        <form action="#">
-          <input onChange={this.handleChanges} type="text" />
-          <button type="submit">Send</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChanges} type="text" value={newTask} />
+          <button type="submit">
+            <FaPlus  size={20} />
+          </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <li key={task}>
+              {task}
+              <div>
+                <FaPencilAlt className="edit"/>
+                <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
